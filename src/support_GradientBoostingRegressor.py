f0ed155e628891c28a59f977d613b71a3448da8d
Alex <alexcastrovarela@gmail.com>
2024-11-27 16:50:25 +0100

    up

--- src/support_GradientBoostingRegressor.py
+++ src/support_GradientBoostingRegressor.py
@@ -28,16 +28,14 @@ def metricas(y_train, y_train_pred, y_test, y_test_pred):
         'r2_score': round(r2_score(y_train, y_train_pred), 4),
         'MAE': round(mean_absolute_error(y_train, y_train_pred), 4),
         'MSE': round(mean_squared_error(y_train, y_train_pred), 4),
-        'RMSE': round(np.sqrt(mean_squared_error(y_train, y_train_pred)), 4),
-        'RMSE-MAE':round(np.sqrt(mean_squared_error(y_test, y_test_pred)), 4)-round(mean_absolute_error(y_test, y_test_pred), 4)
+        'RMSE': round(np.sqrt(mean_squared_error(y_train, y_train_pred)), 4)
     }
     
     test_metricas = {
         'r2_score': round(r2_score(y_test, y_test_pred), 4),
         'MAE': round(mean_absolute_error(y_test, y_test_pred), 4),
         'MSE': round(mean_squared_error(y_test, y_test_pred), 4),
-        'RMSE': round(np.sqrt(mean_squared_error(y_test, y_test_pred)), 4),
-        'RMSE-MAE':round(np.sqrt(mean_squared_error(y_test, y_test_pred)), 4)-round(mean_absolute_error(y_test, y_test_pred), 4)
+        'RMSE': round(np.sqrt(mean_squared_error(y_test, y_test_pred)), 4)
     }
     
     # Calcular diferencias
@@ -55,9 +53,6 @@ def metricas(y_train, y_train_pred, y_test, y_test_pred):
     media_respuesta = round((np.mean(y_train) + np.mean(y_test)) / 2, 4)
     mediana_respuesta = round((np.median(y_train) + np.median(y_test)) / 2, 4)
     
-    ratio_media= {metric:((train_metricas[metric]+test_metricas[metric])/2)/media_respuesta for metric in train_metricas}
-    ratio_mediana= {metric:((train_metricas[metric]+test_metricas[metric])/2)/mediana_respuesta for metric in train_metricas}
-
     # Calcular porcentaje de influencia basado en la referencia
     porcentaje2 = {
         metric: round((abs(diferencias[metric]) / media_respuesta) * 100, 4)
@@ -74,98 +69,14 @@ def metricas(y_train, y_train_pred, y_test, y_test_pred):
     metricas = {
         'Train': train_metricas,
         'Test': test_metricas,
-        'Diferenceia Train-Test': diferencias,
+        'Diferenceias': diferencias,
         'Porcentaje diferencia (%)': porcentaje,
         'Media':media_respuesta,
-        'Ratio Media':ratio_media,
-        'Influencia dif media (%)': porcentaje2,
+        'Influencia media (%)': porcentaje2,
         'Mediana':mediana_respuesta,
-        'Ratio Mediana':ratio_mediana,
-        'Influencia dif mediana (%)': porcentaje3,    
+        'Influencia mediana (%)': porcentaje3,    
     }
     return pd.DataFrame(metricas).T
-
-def graficar_arbol_decision(modelo, nombres_caracteristicas, tamano_figura=(30, 30), tamano_fuente=12):
-    """
-    Grafica un árbol de decisión con opciones personalizables.
-
-    Parámetros:
-        modelo: Árbol de decisión entrenado (DecisionTreeClassifier o DecisionTreeRegressor).
-        nombres_caracteristicas: Lista o índice con los nombres de las características (columnas).
-        tamano_figura: Tuple, tamaño de la figura (ancho, alto).
-        tamano_fuente: Tamaño de la fuente en la gráfica.
-    """
-    plt.figure(figsize=tamano_figura)
-    plot_tree(
-        decision_tree=modelo,
-        feature_names=nombres_caracteristicas,
-        filled=True,  # Colorear los nodos
-        rounded=True,  # Esquinas redondeadas
-        fontsize=tamano_fuente,
-        proportion=True,  # Mostrar proporciones en lugar de valores absolutos
-        impurity=False  # Ocultar impureza de los nodos
-    )
-    plt.show()
-
-def loo_cross_validation_rmse(model, X, y):
-    """
-    Realiza validación cruzada Leave-One-Out (LOO) y calcula el RMSE promedio.
-
-    Parámetros:
-        model: modelo de regresión (ej. LinearRegression de sklearn)
-        X: DataFrame o matriz con las características (features)
-        y: Serie o vector con el objetivo (target)
-    
-    Retorno:
-        float: RMSE promedio obtenido en la validación cruzada
-    """
-    loo = LeaveOneOut()
-    scores = []
-
-    for train_index, test_index in tqdm(loo.split(X), total=len(X)):
-        # Dividir los datos en entrenamiento y prueba
-        X_train_cv, X_test_cv = X.iloc[train_index], X.iloc[test_index]
-        y_train_cv, y_test_cv = y.iloc[train_index], y.iloc[test_index]
-
-        # Entrenar el modelo y predecir
-        model.fit(X_train_cv, y_train_cv)
-        y_pred = model.predict(X_test_cv)
-
-        # Calcular RMSE
-        rmse = np.sqrt(mean_squared_error([y_test_cv.values[0]], y_pred))
-        scores.append(rmse)
-
-    # Calcular y retornar el RMSE promedio
-    return np.mean(scores)
-
-
-def rmse_plot(df, mse_column, columns, figure_size=(16, 10)):
-    """
-    Función sencilla para calcular y graficar el RMSE agrupado por columnas específicas.
-
-    Parámetros:
-    - df: DataFrame que contiene los datos.
-    - mse_column: Columna que contiene los valores de MSE.
-    - columns: Lista de columnas para calcular y graficar el RMSE.
-    - figure_size: Tamaño de la figura (ancho, alto) en tuplas.
-    """
-    num_columns = len(columns)
-    fig, axes = plt.subplots(1, num_columns, figsize=figure_size, sharey=True)
-
-    if num_columns == 1:  # Ajustar para un solo gráfico
-        axes = [axes]
-
-    for ax, column in zip(axes, columns):
-        # Calcular RMSE agrupado
-        rmse = np.sqrt(df.groupby(column)[mse_column].mean().abs())
-
-        # Graficar
-        sns.lineplot(x=rmse.index, y=rmse.values, ax=ax)
-        ax.set_title(f"RMSE por {column}")
-        ax.grid()
-
-    plt.tight_layout()
-    plt.show()
 
 def analizar_correlaciones(df, target_column, threshold=0.05):
     """
